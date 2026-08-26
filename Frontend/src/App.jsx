e0c728c9cb0145c8810d04ea057b11069f50b901
teja-thodeti <saitejathodeti03@gmail.com>
2026-08-26 16:18:29 +0530

    register page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-
+import Register from "./pages/Register"
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
-
+                <Route path="/Register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
